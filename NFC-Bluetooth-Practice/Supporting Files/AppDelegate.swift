43a79cb30e0c447db0f772afae347528541bb80b
Dawei Hao <dwsamurai84@gmail.com>
2025-04-21 23:35:08 +0800

    Add content in AppDelegates.

--- NFC-Bluetooth-Practice/Supporting Files/AppDelegate.swift
+++ NFC-Bluetooth-Practice/Supporting Files/AppDelegate.swift
@@ -10,8 +10,8 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -30,7 +30,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        guard userActivity.activityType == NSUserActivityTypeBrowsingWeb else {
+            return false
+        }
 
+        // Confirm that the NSUserActivity object contains a valid NDEF message.
+        let ndefMessage = userActivity.ndefMessagePayload
+        guard !ndefMessage.records.isEmpty,
+            ndefMessage.records[0].typeNameFormat != .empty else {
+                return false
+        }
+        
+        return true
+    }
 
 }
 
